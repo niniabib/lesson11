c65e9289b8d63ed007adf200e234daa012f00145
niniabib <ninobibichadze1@gmail.com>
2022-03-15 01:50:34 +0400

    java

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+<<<<<<< Updated upstream
 
+=======
+let user = {
+  firstname: 'giorgi',
+  lastname: 'smith',
+  age: 25,
+  studentstatus: 'active'
+}
+console.log(user.studentstatus);
+
+
+
+let numbers=[8,15,30,56];
+for(let x=0; x<numbers.length; x++)
+console.log(numbers[0]);
+for(let x=0; x<numbers.length; x++)
+console.log(numbers[1]);
+for(let x=0; x<numbers.length; x++)
+console.log(numbers[2]);
+for(let x=0; x<numbers.length; x++)
+console.log(numbers[3]);
+
+
+
+let numbers = [5,6,10,25,3,7,4,100,36];
+  for (i=0;i < numbers.length; i++){
+  if (numbers[i]>5) {
+console.log(numbers[i]);
+}
+}
+
+
+
+let user ={
+  name: 'giorgi',
+  age: 20,
+  studentstatus: 'active'
+}
+if (user["age"]>18,user["studentstatus"]='active') {
+  console.log("hello");
+}
+if (user["name"]='giorgi'){
+  console.log("hello giorgi");
+}
+if (user["status"]='active' || user['age']<25){
+  console.log('hello world');
+} else {
+  console.log('error');
+}
+
+
+
+let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];{
+  console.log(array[0]);
+  console.log(array[1]);
+  console.log(array[5]);
+  console.log(array[6]);
+}
+
+
+
+let users =  [
+  {username:'test1', status: 'false'},
+  {username:'test2', status: 'false'},
+  {username:'test3', status: 'true'}
+];
+console.log('true');
+>>>>>>> Stashed changes
